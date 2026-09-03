8f8b4aeece9461568d32ce4d3205f898972c42df
Vasileios Moratis <vassi07@gmail.com>
2026-09-03 14:48:33 +0200

    Add ES Lint config again

--- index.js
+++ index.js
@@ -2,31 +2,33 @@ import * as cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
 
-const html = await fetch("https://memegen-link-examples-upleveled.netlify.app/")
+const html = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
 const text = await html.text();
 const $ = cheerio.load(text);
 
-
-
 // Slice the first ten images and run a map on them to extract the src of each one and put it in an array.
-const imageSrcs = $('img').slice(0,10).map((index, element) => {
-  return $(element).attr('src');
-}).get();
+const imageSrcs = $('img')
+  .slice(0, 10)
+  .map((index, element) => {
+    return $(element).attr('src');
+  })
+  .get();
 
 // Now we have to fetch all the urls that are in the array imageSrcs.
 
-
-for (let i=0; i<imageSrcs.length; i++) {
+for (let i = 0; i < imageSrcs.length; i++) {
   const response = await fetch(imageSrcs[i]);
   const arrayBuffer = await response.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
 
   if (!fs.existsSync('memes')) {
     fs.mkdirSync('memes');
-    }
-  fs.writeFile(path.join('memes', `image0${i+1}.jpg`), buffer, (err) => {
+  }
+  fs.writeFile(path.join('memes', `image0${i + 1}.jpg`), buffer, (err) => {
     if (err) {
       console.error(err);
-    } 
+    }
   });
 }
