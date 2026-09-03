e8b6ba504aa4f2f3dd1ee93ded1553e140509fe5
Vasileios Moratis <vassi07@gmail.com>
2026-09-03 07:41:05 +0200

    Fetch individual files and create folder structure

--- index.js
+++ index.js
@@ -2,54 +2,31 @@ import * as cheerio from 'cheerio';
 import fs from 'fs';
 import path from 'path';
 
-async function fetchCreateFile() {
-
-  const html= await fetch(`https://memegen-link-examples-upleveled.netlify.app` );
-  const htmlText= await html.text();
-  const $ = cheerio.load(htmlText);
-  const firstTenImages = $('img').slice(0,10).map((i, el) => $(el).attr('src')).get();
-
-
-  // For every src in firstTenImages, fetch the image , transform to buffer and save it to the images folder
+const html = await fetch("https://memegen-link-examples-upleveled.netlify.app/")
+const text = await html.text();
+const $ = cheerio.load(text);
 
 
 
- firstTenImages.forEach(async (src, index) => {
+// Slice the first ten images and run a map on them to extract the src of each one and put it in an array.
+const imageSrcs = $('img').slice(0,10).map((index, element) => {
+  return $(element).attr('src');
+}).get();
+
+// Now we have to fetch all the urls that are in the array imageSrcs.
 
 
-  const imageResponse = await fetch(src);
+for (let i=0; i<imageSrcs.length; i++) {
+  const response = await fetch(imageSrcs[i]);
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
 
-
-  const imageBuffer = await imageResponse.arrayBuffer();
-  const buffer = Buffer.from(imageBuffer);
-
-
-
-
-
-// Here we ae checking if the memes/images folder exists, if not we create it. Then we write the image buffer to a file in that folder.
-    if (!fs.existsSync('memes')) {
-      fs.mkdir('memes', { recursive: true });
+  if (!fs.existsSync('memes')) {
+    fs.mkdirSync('memes');
     }
-
-
-
-    fs.writeFileSync(path.join('memes', `image0${index+1}.jpg`), buffer);
-  })}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fetchCreateFile();
-
+  fs.writeFile(path.join('memes', `image0${i+1}.jpg`), buffer, (err) => {
+    if (err) {
+      console.error(err);
+    } 
+  });
+}
